a981fd009275f7ec2540fc32349d4b27825dfabf
Florian Lopez Plaza <contact@infinity-fx.com>
2024-04-09 23:03:02 +0200

    Color theme.

--- fluid.config.js
+++ fluid.config.js
@@ -3,6 +3,19 @@ module.exports = {
     theme: {
         font: {
             family: 'var(--font-manrope)'
+        },
+        palettes: {
+            light: {
+                primary: ['#d67922', '#dd8f47', '#e4a66c', '#ebbc91', '#f1d2b5', '#f8e9da'],
+                accent: ['#000000'],
+                fg: ['#ffffff', '#f2edeb'],
+                bg: ['#f7f6f5']
+            },
+            dark: {
+                primary: ['#d67922', '#b2651c', '#8f5117', '#6b3d11', '#47280b', '#241406'],
+                fg: ['#141414', '#1f1f1f'],
+                accent: ['#ffffff']
+            }
         }
     }
 }
